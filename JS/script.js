c6a3c9e4a706d57feadf9eb943ed7898e44ba5ba
usman4li <135771284+usman4li@users.noreply.github.com>
2023-06-07 07:16:25 +0700

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+var text = "Hello World!"
+
+document.getElementById("kirim").addEventListener('click')
+    console.log(text);
+})
+
+document.getElementsByName('nama').addEventListener('input',function() {
+    console.log("sudah bisa diinput")
+})

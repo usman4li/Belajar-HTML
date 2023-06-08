9bedc7f1d292f1ffc9358951ab71bd73b7948f7e
usman4li <135771284+usman4li@users.noreply.github.com>
2023-06-08 20:34:19 +0700

    Delete JS Directory

--- JS/script.js
+++
@@ -1,9 +0,0 @@
-var text = "Hello World!"
-
-document.getElementById("kirim").addEventListener('click')
-    console.log(text);
-})
-
-document.getElementsByName('nama').addEventListener('input',function() {
-    console.log("sudah bisa diinput")
-})
